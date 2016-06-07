0c5dba323319e9934e5743ce1d1c894e17fe3cfc
kritikakrishnan <kritikagopalakrishnan@gmail.com>
2016-06-07 12:33:56 -0700

    Tabbed activity with Navigation drawer

---
+++ app/src/main/java/com/garcon/garcon/homeactivity.java
@@ -0,0 +1,80 @@
+package com.garcon.garcon;
+
+
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class homeactivity extends AppCompatActivity{
+    DrawerLayout myDrawerLayout;
+    NavigationView myNavigationView;
+    FragmentManager myFragmentManager;
+    FragmentTransaction myFragmentTransaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homeactivity);
+
+        /**
+         *Setup the DrawerLayout and NavigationView
+         */
+
+        myDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        myNavigationView = (NavigationView) findViewById(R.id.profile) ;
+
+        /**
+         * Lets inflate the very first fragment
+         * Here , we are inflating the TabFragment as the first Fragment
+         */
+
+        myFragmentManager = getSupportFragmentManager();
+        myFragmentTransaction = myFragmentManager.beginTransaction();
+        myFragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
+        /**
+         * Setup click events on the Navigation View Items.
+         */
+
+        myNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                myDrawerLayout.closeDrawers();
+
+
+
+                if (menuItem.getItemId() == R.id.nav_item_sent) {
+                    FragmentTransaction fragmentTransaction = myFragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.containerView,new SentFragment()).commit();
+
+                }
+
+                if (menuItem.getItemId() == R.id.nav_item_inbox) {
+                    FragmentTransaction xfragmentTransaction = myFragmentManager.beginTransaction();
+                    xfragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
+                }
+
+                return false;
+            }
+
+        });
+
+        /**
+         * Setup Drawer Toggle of the Toolbar
+         */
+
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this,myDrawerLayout, toolbar,R.string.app_name,
+                R.string.app_name);
+
+        myDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mDrawerToggle.syncState();
+
+    }
+}

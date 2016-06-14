f306721630a771a3c9b82a005032d28de2580144
mynktwri <mayank@rockinfamily.com>
2016-06-13 18:28:54 -0700

    Continued work on search system

--- app/src/main/java/com/garcon/garcon/SearchActivity.java
+++ app/src/main/java/com/garcon/garcon/SearchActivity.java
@@ -8,6 +8,7 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -22,24 +23,11 @@ public class SearchActivity extends ListActivity {
     private TextView text;
     private ListView listView;
     private List<String> list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
-        listView = (ListView) findViewById(R.id.list);
-        list = new ArrayList<String>();
-        text = (TextView) findViewById(R.id.actionText);
-        list.add("hi");
-        list.add("hi");
-        list.add("hi");
-        list.add("hi");list.add("hi");
-        list.add("hi");
-        list.add("hi");
-
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, R.layout.second_layout,list);
-        listView.setAdapter(adapter);
-
 
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
@@ -48,16 +36,8 @@ public class SearchActivity extends ListActivity {
         }
     }
 
-    protected void onListItemClick(ListView list, View view, int position, long id){
-        super.onListItemClick(list, view, position, id);
-
-        String selectedItem = (String) getListView().getItemAtPosition(position);
-        text.setText("clicked " + selectedItem + " at position " + position);
-
-    }
-
     protected void search (String query){
         //TODO: search based on data storage
     }
 
-}
+    }
--- app/src/main/java/com/garcon/garcon/SecondFragment.java
+++ app/src/main/java/com/garcon/garcon/SecondFragment.java
@@ -2,7 +2,9 @@ package com.garcon.garcon;
 
 /**
  * Created by kritikagopalakrishnan on 6/2/16.
+ * Edited by Mayank Tiwari
  */
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,13 +13,28 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SecondFragment extends Fragment {
 
+    private TextView text;
+    private ListView LV;
+    private List<String> list;
+
     @Nullable
     @Override
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         return inflater.inflate(R.layout.second_layout, null);
     }
 

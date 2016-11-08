15dc14c8b2bbb4c119b0cea2a7770632a2f9132f
ccasil <ccasil@ucsc.edu>
2016-11-07 16:53:51 -0800

    Settings
    Added settings page/terms and conditions

--- app/src/main/java/com/garcon/garcon/termsandconditions.java
+++ app/src/main/java/com/garcon/garcon/termsandconditions.java
@@ -1,8 +1,43 @@
 package com.garcon.garcon;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 /**
  * Created by cesar_000 on 11/6/2016.
  */
 
-public class termsandconditions {
+public class termsandconditions extends AppCompatActivity {
+    private static final String TAG = termsandconditions.class.getName();
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_terms_conditions);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    //System.out.println("onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    //System.out.println("onAuthStateChanged:signed_out");
+                    finish();
+                }
+            }
+        };
+    }
+//    android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.terms_toolbar);
 }

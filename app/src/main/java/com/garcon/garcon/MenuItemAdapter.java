5e3614a970f730c62e70593e5cf9cb5db3fc046c
aakashtrivedi1992 <aakasht10@yahoo.com>
2018-01-24 17:15:26 -0800

    Updated Add to Cart functionality

--- app/src/main/java/com/garcon/garcon/MenuItemAdapter.java
+++ app/src/main/java/com/garcon/garcon/MenuItemAdapter.java
@@ -1,5 +1,4 @@
 package com.garcon.garcon;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -14,9 +13,13 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.TextView;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import java.util.List;
-
+import java.util.ArrayList;
+import static android.app.PendingIntent.getActivity;
+import static android.content.Intent.getIntent;
 public class MenuItemAdapter extends BaseAdapter implements ListAdapter {
 
     public final static String LOG_TAG = MenuItemAdapter.class.getSimpleName();
@@ -25,6 +28,11 @@ public class MenuItemAdapter extends BaseAdapter implements ListAdapter {
     public Context con;
     View view;
     private String curr_rest;
+    private MenuItem item;
+    private int numOrdered = 1;
+    boolean fromMenu=true;
+    private int position;
+    String restaurantname;
 
     public MenuItemAdapter(List<MenuItem> list, Context context) {
         this.list = list;
@@ -55,6 +63,38 @@ public class MenuItemAdapter extends BaseAdapter implements ListAdapter {
         //just return 0 if your list items do not have an Id variable.
         return 0;
     }
+    public void setUpButtons(){
+
+        final TextView tv = (TextView) view.findViewById(R.id.numItems);
+        tv.setText(String.valueOf(numOrdered));
+
+
+
+
+
+    }
+
+    public boolean checkMinMax(){
+      /* ArrayList<MenuItem.ModifierGroup> mgList=null;
+        try{
+         mgList = item.getModifierGroups();}
+        catch(Exception e)
+        {
+            String a=e.getMessage().toString();
+        }
+        for(int i = 0; i < mgList.size(); i++){
+            MenuItem.ModifierGroup mgrp = mgList.get(i);
+            int min = mgrp.getMinimum();
+            int max = mgrp.getMaximum();
+            int num_checked = 0;
+            for(MenuItem.ModifierGroup.ItemModifier im : mgrp.getModifierList())
+                if(im.isAdded())
+                    num_checked++;
+            if((num_checked<min || num_checked>max) && mgrp.isRequired())
+                return false;
+        }*/
+        return true;
+    }
 
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
@@ -82,19 +122,19 @@ public class MenuItemAdapter extends BaseAdapter implements ListAdapter {
         int height = dm.heightPixels;
         listItemText.setHeight((int)(0.12*height));
         listItemText.setOnClickListener(new View.OnClickListener(){
-                                             @Override
-                                             public void onClick(View v) {
-                                                 //TODO blur or dim background
-                                                 Log.i(LOG_TAG, "menu item selected");
-                                                 Intent intent = new Intent(context, MainItemActivity.class);
-                                                 Bundle extras = new Bundle();
-                                                 extras.putSerializable("menu-item", list.get(position));
-                                                 intent.putExtras(extras);
+            @Override
+            public void onClick(View v) {
+                //TODO blur or dim background
+                Log.i(LOG_TAG, "menu item selected");
+                Intent intent = new Intent(context, MainItemActivity.class);
+                Bundle extras = new Bundle();
+                extras.putSerializable("menu-item", list.get(position));
+                intent.putExtras(extras);
                 intent.putExtra("currentrestaurant",curr_rest);
-                                                 context.startActivity(intent);
-                                                 notifyDataSetChanged();
-                                             }
-                                         });
+                context.startActivity(intent);
+                notifyDataSetChanged();
+            }
+        });
 
       /*  listItemText.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -111,7 +151,43 @@ public class MenuItemAdapter extends BaseAdapter implements ListAdapter {
         }); */
 
         notifyDataSetChanged();
+
+        Button addButton = (Button) view.findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add item to viewcart
+                /*Intent intent = new Intent(context, MainItemActivity.class);
+                Bundle extras = new Bundle();
+                extras.putSerializable("menu-item", list.get(position));
+                intent.putExtras(extras);
+                intent.putExtra("currentrestaurant",curr_rest);
+                context.startActivity(intent);*/
+
+
+
+                if (numOrdered > 0 && checkMinMax()) {
+
+                    item=list.get(position);
+                    //change num_ordered, special instructions,all modifiers
+                    OrderSingleton.getInstance().addItem(item,numOrdered,"");
+
+                    Log.i(LOG_TAG, "item added");
+                }
+
+
+                //Intent intent = new Intent(MainItemActivity.this, MainMenuActivity.class);
+                //MainItemActivity.this.startActivity(intent);
+
+            }
+        });
+
+        notifyDataSetChanged();
+        //setUpButtons();
+        //checkMinMax();
         return view;
+
+
     }
 
 

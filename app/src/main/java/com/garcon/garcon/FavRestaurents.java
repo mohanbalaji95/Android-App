8a18f50023d7ebe9e4be1fa04eafe99f5734ad67
rafi <xmohdrafi@gmail.com>
2018-06-08 14:44:54 -0700

    fixed restaurent fav

---
+++ app/src/main/java/com/garcon/garcon/FavRestaurents.java
@@ -0,0 +1,11 @@
+package com.garcon.garcon;
+
+public class FavRestaurents {
+    String name;
+    boolean fav_status;
+    FavRestaurents(String name, boolean fav_status){
+        this.name = name;
+        this.fav_status=fav_status;
+    }
+
+}
